5d2874ab13c8223cc74e5c484421364b51178961
89674912810 <ddbash@icloud.com>
2026-02-10 18:21:24 +0300

    done shit

--- yandexapi.py
+++ yandexapi.py
@@ -9,7 +9,7 @@ static_apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
 org_apikey = "72e09cfa-9163-4f5c-ba75-916fb947567b"
 
 
-def get_static(api=static_apikey, ll='37.677751,55.757718', spn="", pt=''):
+def get_static(api=static_apikey, ll='37.677751,55.757718',  pt=''):
     return requests.get("https://static-maps.yandex.ru/v1", params={
         'apikey': api,
         'll': ll,
