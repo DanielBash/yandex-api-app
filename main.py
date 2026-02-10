6c7870d4e5c6bc4ded4fb95cce66b41457d9704d
89674912810 <ddbash@icloud.com>
2026-02-10 18:12:32 +0300

    Added basic structure

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+import sys
+from PyQt6.QtWidgets import QWidget, QLineEdit, QPushButton, QApplication
+
+
+class YandexApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Pyqt6 App api')
+        self.setGeometry(300, 300, 500, 500)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = YandexApp()
+    window.show()
+    sys.exit(app.exec())
